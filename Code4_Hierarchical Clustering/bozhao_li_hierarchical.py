14297438c38959c26ce143473d0233b1f384653b
Bozhao Li <youhusky@126.com>
2015-11-10 18:49:26 -0800

    final

--- Code4_Hierarchical Clustering/bozhao_li_hierarchical.py
+++ Code4_Hierarchical Clustering/bozhao_li_hierarchical.py
@@ -1,26 +1,47 @@
 import heapq
 from math import sqrt
+from itertools import combinations
+import sys
+
 __author__ = 'Joshua'
 
 
 def main():
-	raw_data = get_data()
-	raw_merge = pair(raw_data)
-	merge_heap(raw_merge, raw_data)
+	file_name = sys.argv[1]
+	number = sys.argv[2]
+
+	raw_data = get_data(file_name)
+	standard_value = standard_class(raw_data)
+	raw_merge, value = pair(raw_data)
+	result = get_items_distance(raw_merge, value, int(number))
+	evaluate(result, standard_value)
+	for each_calss in result:
+		print each_calss
 
 
-def get_data():
+def get_data(name):
 	"""
 
 	:rtype: object
 	"""
-	with open('iris.dat') as filename:
+	with open(name) as filename:
 		opendata = filename.readlines()
-		read_data = []
-		for eachline in opendata:
-			read_data.append(eachline.strip().split(','))
+		read_data = [eachline.strip().split(',') for eachline in opendata]
 
 	return read_data
+
+
+def standard_class(raw_data):
+	var = [raw_data[classname][4] for classname in range(len(raw_data))]
+	var = list(set(var))
+	object_standard_class = []
+	for each_classname in range(len(var)):
+		temp = []
+		for object_index in range(len(raw_data)):
+			if raw_data[object_index][4] == var[each_classname]:
+				temp.append(object_index)
+		object_standard_class.append(temp)
+	return object_standard_class
 
 
 def pair(data):
@@ -28,82 +49,128 @@ def pair(data):
 
 	:rtype: object
 	"""
-	merge = []
+	merged_distance = []
+	object_value = []
 
-	for i in range(len(data)):
-		for j in range(i + 1, len(data)):
-			merge.append([euclidean_distance(data[i][:4], data[j][:4]), [i, j]])
-	return merge
-
-
-def merge_heap(data, raw):
-	heap_list = []
-	for k in data:
-		heapq.heappush(heap_list, k)
-	merged_list = []
-	tmv,tmq = heapq.heappop(heap_list)
-	merged_list.append(tmq)
-	while 1:
-		distance, merged_items = heapq.heappop(heap_list)
-		flag = 0
-		for i in merged_list:
-			tmp = [val for val in i if val in merged_items]
-			if tmp == i:
-				flag = 0
-				break
-			elif len(tmp) > 0 and tmp != i:
-				flag = 1
-				break
-			else:
-				flag = 1
-		if flag == 1:
-			merged_list.append(merged_items)
-			add_list = []
-			for j in range(len(raw)):
-				if j in merged_items:
-					pass
-				else:
-					for k in range(len(merged_items)):
-						min_value = []
-						min_value.append(euclidean_distance(raw[merged_items[k]][:4], raw[j][:4]))
-					new_merged = merged_items[:]
-					new_merged.append(j)
-					add_list.append([min(min_value), new_merged])
-			heap_list.extend(add_list)
-			new = []
-			for each in merged_list:
-				if set(tuple(merged_items)).issuperset(set(tuple(each))):
-					pass
-				else:
-					new.append(each)
-
-			for i in range(len(merged_list)-1):
-				if set(tuple(merged_items)).intersection(set(tuple(merged_list[i]))):
-					#print set(tuple(merged_items)).intersection(set(tuple(merged_list[i])))
-
-					new.remove(merged_list[i])
-
-					merged_items = list(set(tuple(merged_items)).union(set(tuple(merged_list[i]))))
-
-				else:
-					pass
-			new.append(merged_items)
-			merged_list = new
-			print 'a', merged_list
-
-		else:
-			pass
-
-
+	for object_index_i in range(len(data)):
+		object_value.append([object_index_i, data[object_index_i][:4]])
+		for object_index_j in range(object_index_i + 1, len(data)):
+			merged_distance.append([euclidean_distance(data[object_index_i][:4],
+			                                 data[object_index_j][:4]),
+			                              [object_index_i, object_index_j]])
+	return merged_distance, object_value
 
 
 def euclidean_distance(vector1, vector2):
+	return sqrt(sum([pow((float(vector1[i]) - float(vector2[i])), 2) for i in range(len(vector1))]))
 
-	t_sum = sum([pow((float(vector1[i]) - float(vector2[i])), 2) for i in range(len(vector1))])
-	s_sum = sqrt(t_sum)
-	return s_sum
+
+def get_items_distance(raw_data, raw_value, number):
+	def get_centroid(part1, part2, item_list):
+		final_join_object = []
+		if type(part1) == int and type(part2) == int:
+			for each_object in range(len(item_list[part1])):
+				final_join_object.append((float(item_list[part1][each_object])
+				                    + float(item_list[part2][each_object])) / 2.0)
+			return final_join_object
+		elif type(part1) == int and type(part2) != int:
+			for each_object in part2:
+				final_join_object.append(each_object)
+			final_join_object.append(part1)
+			final_value = []
+			for each_object in range(len(item_list[each_object])):
+				temp = []
+				for dimension in range(len(final_join_object)):
+					temp.append(float(item_list[final_join_object[dimension]][each_object]))
+				final_value.append(sum(temp) / len(final_join_object))
+			return final_value
+		elif type(part1) != int and type(part2) == int:
+			for each_object in part1:
+				final_join_object.append(each_object)
+			final_join_object.append(part2)
+			final_value = []
+			for each_object in range(len(item_list[each_object])):
+				temp = []
+				for dimension in range(len(final_join_object)):
+					temp.append(float(item_list[final_join_object[dimension]][each_object]))
+				final_value.append(sum(temp) / len(final_join_object))
+			return final_value
+		else:
+			for each_object in part1:
+				final_join_object.append(each_object)
+			for dimension in part2:
+				final_join_object.append(dimension)
+			final_value = []
+			for each_object in range(len(item_list[each_object])):
+				temp = []
+				for dimension in range(len(final_join_object)):
+					temp.append(float(item_list[final_join_object[dimension]][each_object]))
+				final_value.append((sum(temp) / len(final_join_object)))
+			return final_value
+
+	data = raw_data
+	item_list = [object[1] for object in raw_value]
+	merge_dict = {}
+	for object in raw_value:
+		merge_dict[object[0]] = object[1]  # dict save (2,3):[1.1,2.2,3.3,4.4]
+	heap_list = []
+	for each_heap in data:
+		heapq.heappush(heap_list, each_heap)
+	distance, merged_items = heapq.heappop(heap_list)
+
+	while len(merge_dict) > number:
+		'''
+		tuple union: three part int + (), ()+int and ()()
+		'''
+		if type(merged_items[0]) == tuple and type(merged_items[1]) == tuple:
+			merge_dict[tuple(sorted(set(merged_items[0]).union(set(merged_items[1]))))] = \
+				get_centroid(merged_items[0], merged_items[1], item_list)
+
+		elif type(merged_items[0]) == tuple or type(merged_items[1]) == tuple:
+			if type(merged_items[0]) == tuple and type(merged_items[1]) != tuple:
+				temp = [merged_items[1]]
+				for num in merged_items[0]:
+					temp.append(num)
+				merge_dict[tuple(temp)] = \
+					get_centroid(merged_items[0], merged_items[1], item_list)
+
+			elif type(merged_items[1]) == tuple and type(merged_items[0]) != tuple:
+				temp = [merged_items[0]]
+				for num in merged_items[1]:
+					temp.append(num)
+				merge_dict[tuple(temp)] = \
+					get_centroid(merged_items[0], merged_items[1], item_list)
+		else:
+			merge_dict[tuple(sorted(merged_items))] = \
+				get_centroid(merged_items[0], merged_items[1], item_list)
+
+		merge_dict.pop(merged_items[0])
+		merge_dict.pop(merged_items[1])
+
+		heap_list = []
+		for object in merge_dict:
+			for j in merge_dict:
+				if object != j:
+					heap_list.append([euclidean_distance(merge_dict[object], merge_dict[j]), [object, j]])
+		tep = []
+		for each_heap in heap_list:
+			heapq.heappush(tep, each_heap)
+		distance, merged_items = heapq.heappop(tep)
+	return [sorted(list(object)) for object in merge_dict]
+
+
+def evaluate(result, standard_value):
+	def get_list(value):
+		temp = [list(combinations(i, 2)) for i in value]
+		return [each_classvalue for each_class in range(len(temp)) for each_classvalue in temp[each_class]]
+
+	standard = get_list(standard_value)
+	test = get_list(result)
+	tp = set(standard).intersection(set(test))
+	precision = float(len(tp)) / float(len(test))
+	recall = float(len(tp)) / float(len(standard))
+	print precision, '\n', recall
+
 
 if __name__ == '__main__':
 	main()
-
-
